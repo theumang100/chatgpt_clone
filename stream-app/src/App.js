8431661447f745ee256dbfe49da073f93f8883da
theumang100 <theumangpatelu2p@gmail.com>
2023-07-18 13:46:45 -0700

    env removed

--- stream-app/src/App.js
+++ stream-app/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [response, setResponse] = useState('');
+
+  const handleGenerate = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/generate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ prompt: 'Hello' }),
+      });
+
+      const reader = response.body.getReader();
+      let chunks = '';
+
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        chunks += new TextDecoder().decode(value);
+        setResponse(chunks);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleGenerate}>Generate</button>
+      <pre>{response}</pre>
     </div>
   );
 }
