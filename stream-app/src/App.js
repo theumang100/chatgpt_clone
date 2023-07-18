05f0114917cac86becad8384e212365a63410b08
theumang100 <theumangpatelu2p@gmail.com>
2023-07-18 13:11:22 -0700

    frontend added

---
+++ stream-app/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [response, setResponse] = useState('');
+
+  const handleGenerate = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/generate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ prompt: 'hello' }),
+      });
+  
+      const reader = response.body.getReader();
+      let chunks = '';
+  
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        chunks += new TextDecoder().decode(value);
+        setResponse(chunks);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleGenerate}>Generate</button>
+      <pre>{response}</pre>
+    </div>
+  );
+}
+
+export default App;
